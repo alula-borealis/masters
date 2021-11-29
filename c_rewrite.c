426a7523c9d23c5689e712dce908fd608eac0ed6
rafamadriz <rafaelcontacto@pm.me>
2021-11-29 17:58:20 +0000

    expanding green function with gsl matrices

--- c_rewrite.c
+++ c_rewrite.c
@@ -4,16 +4,19 @@
 #include <time.h>
 #include <complex.h>
 #include <gsl/gsl_linalg.h>
-#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_matrix.h>
 
 #define _USE_MATH_DEFINES
 
+// global variables
+int n = 10;
+
 double incident(double x);
-double green_function(double r1, double r2);
+double green_function(double r1[n][3], double r2[n][3]);
 
 int main()
 {
-	int i, j, k, n = 10;
+	int i, j, k;
 	double x, y, z;
 	double scatterers[n][3];
 
@@ -36,6 +39,7 @@ int main()
 	printf("Incident field at scatterer %d: %lf\n", i, field_scatterer);
 	
 	}
+	double green = green_function(scatterers, scatterers);
 	//double field_decomp = gsl_linalg_LU_decomp(green_function)
 }
 
@@ -46,8 +50,11 @@ double incident(double x)
 	return E_0*cexp(1*I*2*M_PI*x);
 }
 
-/*double green_function(double r1, double r2)
+double green_function(double r1[n][3], double r2[n][3])
 {
+	int i, j;
+	gsl_matrix * m = gsl_matrix_alloc (n,n);
+	
 	double r, g;
 	r = r1 - r2;
 
@@ -60,4 +67,21 @@ double incident(double x)
 		g = exp(1*I*2*M_PI*r)/(4*M_PI*r);
 	}
 
-} */
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			gsl_matrix_set (m, i, j, g);
+		}
+	}
+	
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf ("m(%d,%d) = %g\n", i, j,
+					gsl_matrix_get (m, i, j));
+		}
+	}
+	gsl_matrix_free (m);
+}
